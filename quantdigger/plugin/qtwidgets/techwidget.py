3aeab40d0e2b89646ea6f202d4a1e6ba6deb19f1
ongbe <123061031@qq.com>
2015-03-31 20:04:12 +0800

    Update techwidget.py
    获取焦点，按键缩放视图

--- quantdigger/plugin/qtwidgets/techwidget.py
+++ quantdigger/plugin/qtwidgets/techwidget.py
@@ -2,6 +2,7 @@
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg
 from plugin.mplotwidgets.techmplot import TechMPlot
 import matplotlib.pyplot as plt
+from PyQt4.QtCore import Qt
 
 class TechWidget(TechMPlot, FigureCanvasQTAgg):
     def __init__(self, parent=None, *args):
@@ -9,3 +10,5 @@ class TechWidget(TechMPlot, FigureCanvasQTAgg):
         FigureCanvasQTAgg.__init__(self, self.fig)
         TechMPlot.__init__(self, self.fig, *args)
         self.setParent(parent)
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.setFocus()
